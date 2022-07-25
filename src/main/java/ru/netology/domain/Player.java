e16e6c4b5f79dba5835185076c0d13903124df61
Er1kus <goryaev86@mail.ru>
2022-07-25 21:37:59 +0300

    HashMapUsed

--- src/main/java/ru/netology/domain/Player.java
+++ src/main/java/ru/netology/domain/Player.java
@@ -1,62 +1,13 @@
 package ru.netology.domain;
 
+import lombok.*;
 
-import java.util.Objects;
-
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
 public class Player {
     private int id;
     private String name;
     private int strength;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getStrength() {
-        return strength;
-    }
-
-    public void setStrength(int strength) {
-        this.strength = strength;
-    }
-
-    public Player(int id, String name, int strength) {
-        this.id = id;
-        this.name = name;
-        this.strength = strength;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Player player = (Player) o;
-        return id == player.id && strength == player.strength && Objects.equals(name, player.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, strength);
-    }
-
-    @Override
-    public String toString() {
-        return "Player{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", strength=" + strength +
-                '}';
-    }
 }
+
